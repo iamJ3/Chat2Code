dc38f099635dd3f3d976dfdfa35a81c6f15e5764
Jatin Sharma <jatina9410@gmail.com>
2025-07-08 00:20:10 +0530

    removed vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import AppRoutes from './routes/AppRoutes'
 import { UserProvider } from './context/User.context'
-import { Analytics } from "@vercel/analytics/next"
 const App = () => {
   return (
     <UserProvider>
       <div>
         <AppRoutes />
-        <Analytics /> 
       </div>
     </UserProvider>
   )
