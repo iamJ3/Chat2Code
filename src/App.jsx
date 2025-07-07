a2644dc907465aa209d74b6e97e85eaeb093dce3
Jatin Sharma <jatina9410@gmail.com>
2025-07-08 00:08:17 +0530

    set up vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AppRoutes from './routes/AppRoutes'
 import { UserProvider } from './context/User.context'
+import { Analytics } from "@vercel/analytics/next"
 const App = () => {
   return (
     <UserProvider>
       <div>
         <AppRoutes />
+        <Analytics /> 
       </div>
     </UserProvider>
   )
